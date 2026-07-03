827736ee48161258a26a381cd798104cace9e9f4
Muhammad Azeem Hassan <mazeemhassanalvi@gmail.com>
2026-07-03 22:50:59 +0500

    First commit

---
+++ 1st_class.py
@@ -0,0 +1,100 @@
+"""print("Hello World")
+print("Hello World")
+print("Hello World")
+
+n1=int(input("Enter an integer"))
+n2=int(input("Enter an integer"))
+n2=input("Enter another integer")
+n3=(n1 + n2)
+print(n3)
+
+n1=2
+n2=3
+print(n1+n2)
+print(n1*n2)
+print(n1/n2)
+print(n1-n2)
+print(n1//n2)
+print(n1%n2)
+
+n1=10
+n2=1
+n3=12
+print(n1>n2)
+print(n1<n2)
+print(n1>n2 and n2<n3)
+print(n1>n2 or n2<n3)
+
+n1=10
+n2=10
+if n1==n2:
+    print("Numbers are equal")
+else:
+    print("Numbers are not equal")
+
+
+n1=40
+n2=20
+n3=30
+if n1>n2 and n1>n3:
+    print("n1 is greater than n2 and n3")
+else:
+    print("Not greater")
+
+Operators= input("Enter an operator    +, -, / or *")
+if Operators == "+":
+    print("You entered +Operator")
+elif Operators == "-":
+    print("You entered -Operator")
+elif Operators == "*":
+    print("You entered *Operator")
+else:
+    print("You entered / Operator")
+
+n1=int(input("Enter an number"))
+n2=int(input("Enter another number"))
+operator=input("Enter an operator")
+if operator == "+":
+    print (n1+n2)
+elif operator == "-":
+    print(n1-n2)
+elif operator == "*":
+    print(n1*n2)
+elif operator == "/":
+    print(n1/n2)
+else:
+    print("Operator not found")"""
+
+
+Choice= input("Do you like tea or Coffee")
+if Choice == "Coffee":
+    Choice2 = input("Do you like milk in coffee: Yes or no")
+    if Choice2 == "Yes":
+        print("Milk added")
+    else:
+        print("Milk not added")
+elif Choice == "tea":
+    Choice3=input("Do you like sugar in tea or coffee: Yes or no")
+    if Choice3 == "Yes":
+        print("Sugar in tea added")
+    else:
+        print("Sugar is not added")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
