cca5921aed72197ca6a35670799032e6606672e9
Muhammad Azeem Hassan <mazeemhassanalvi@gmail.com>
2026-07-08 20:11:17 +0500

    Loops

--- Loops.py
+++ Loops.py
@@ -25,10 +25,21 @@ for value in range(1,11):
     print(f" 2*{value} = {2*value}")
 
 for i in range(1,6):
-      print("*"* i)"""
+      print("*"* i)
 
 for i in range(1,6):
     print("*"* (6-i))
+
+
+row=5
+for i in range(1,row+1):
+    print(" " * (row-i) + "*" * (2*i-1))"""
+
+for i in range(1,6):
+    for j in range(1,6):
+        print( i, end=" ")
+    print()
+
 
 
 
