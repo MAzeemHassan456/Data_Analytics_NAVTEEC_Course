c19c91c5654464c27851e93a06a1c57b32461f85
Muhammad Azeem Hassan <mazeemhassanalvi@gmail.com>
2026-07-08 19:34:50 +0500

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,37 @@
+"""n=0
+while n<=10:
+ print(n)
+ n+=2
+
+
+while True:
+   age = int(input("Enter your age"))
+   if age>=18:
+       break
+   else:
+      print(age)
+      pass
+
+for i in range(1,10):
+   print(i)
+
+for i in range(1,15,2):
+    print(i)
+
+for i in range(2,21,2):
+    print(i)
+
+for value in range(1,11):
+    print(f" 2*{value} = {2*value}")
+
+for i in range(1,6):
+      print("*"* i)"""
+
+for i in range(1,6):
+    print("*"* (6-i))
+
+
+
+
+
+
