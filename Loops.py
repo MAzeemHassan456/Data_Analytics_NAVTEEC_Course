2000d640284b9f894fc8f9cddcbe9fc0ec436074
Muhammad Azeem Hassan <mazeemhassanalvi@gmail.com>
2026-07-09 19:42:30 +0500

    solve problems

--- Loops.py
+++ Loops.py
@@ -33,12 +33,58 @@ for i in range(1,6):
 
 row=5
 for i in range(1,row+1):
-    print(" " * (row-i) + "*" * (2*i-1))"""
+    print(" " * (row-i) + "*" * (2*i-1))
 
 for i in range(1,6):
     for j in range(1,6):
         print( i, end=" ")
     print()
+
+for i in range(0,51):
+    if i%2==0:
+        print(i)
+
+for value in range(1,11):
+    print(f" 2*{value} = {2*value}
+
+while True:
+    age = int(input("Enter your age"))
+    if age>=18:
+        break
+    else:
+        continue
+
+
+n=int(input("Enter any number"))
+for i in range(1,n+1):
+     print(f" ======== Printing table of {i} ======= ")
+     for j in range(1,11):
+        print(f" {i}*{j} = {i*j}")
+
+row=5
+for i in range(1,row+1):
+    print(" " * (row-i) + "*" * (2*i-1))
+
+
+
+for i in range(1,6):
+      print("*"* i)"""
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print("*", end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
